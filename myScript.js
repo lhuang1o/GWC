c26e5955730adbf538aaa41fa4d1a4353587069d
lhuang1o <41088981+lhuang1o@users.noreply.github.com>
2018-07-10 15:24:54 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,13 @@
+//Javascripts for my web page
+
+function myFunction(){
+  alert("Page is loaded");
+}
+
+/*{<button type="button"
+onclick="document.getElementById('demo').style.fontSize= '35px'">About
+}*/
+
+function changeImage(){
+  document.getElementById("daisy").src="daisyOpen.jpg";
+}
